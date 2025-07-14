d65dad2d299c5181faf0d1e5bf2ae8a207697d07
maneesha465 <maneesha011235@gmail.com>
2025-07-14 21:59:49 +0530

    third commit

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,6 +4,8 @@ import {
   loginUser,
   getUserInfo,
 } from "../controllers/authController.js";
+import { protect } from "../middleware/authMiddleware.js";
+import { upload } from "../middleware/uploadMiddleware.js";
 
 
 
@@ -11,6 +13,16 @@ const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-// router.get("/getuser", protect, getUserInfo);
+router.get("/getuser", protect, getUserInfo);
+
+router.post("/upload-image", upload.single("image"),(req,res) =>{
+  if(!req.file){
+    return res.status(400).json({message:"No file uploaded"})
+  }
+const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${
+  req.file.filename
+}`;
+res.status(200).json({imageUrl})
+});
 
 export default router;
--- server.js
+++ server.js
@@ -5,6 +5,15 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 
+import { fileURLToPath } from 'url';
+
+
+// Fix for __dirname in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+
 dotenv.config();
 
 const app = express();
@@ -24,6 +33,9 @@ connectDB();
 
 // Routes
 app.use("/api/v1/auth", authRoutes);
+
+//server uploads folder
+app.use("/uploads",express.static(path.join(__dirname,"uploads")));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
