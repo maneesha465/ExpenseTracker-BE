67e6f085e4b9d309b8478a2b0e234a51d44545a2
maneesha465 <maneesha011235@gmail.com>
2025-08-19 18:32:31 +0530

    server change from

--- server.js
+++ server.js
@@ -47,6 +47,10 @@ app.use("/uploads",express.static(path.join(__dirname,"uploads")));
 
 
 const PORT = process.env.PORT || 8000;
+app.get("/", (req, res) => {
+  res.send("API is running 🚀");
+});
+
 app.listen(PORT, () =>{
   console.log(`Server running on port ${PORT}`);
 });
